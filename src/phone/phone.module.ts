0c211ffe3808a3dc2c4224802774583022387dc8
TafadzwaD <tafadzwademba@gmail.com>
2025-09-04 22:06:52 +0100

    export phone, service so that it can be used by modules, importing the phone module

--- src/phone/phone.module.ts
+++ src/phone/phone.module.ts
@@ -3,6 +3,7 @@ import { PhoneService } from './phone.service';
 import { PhoneGateway } from './phone.gateway';
 
 @Module({
+  exports: [PhoneService],
   providers: [PhoneGateway, PhoneService],
 })
 export class PhoneModule {}
